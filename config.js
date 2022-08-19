01c53f034b0b54abebc862f189bdf85bfa4243df
Muhammad Faizan <109722241+m-faizan121@users.noreply.github.com>
2022-08-19 16:06:09 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ff5512d0-e752-4942-99b9-c86c7506e5ea';
+export const MODAL_CLOSE_SEC = 2.5;
